c6ad7dcb6a8299dfccee997d18ca452a35613238
abhinavmaurya <maurya.abhinav20@gmail.com>
2016-03-18 12:53:41 -0400

    server fix

--- server.js
+++ server.js
@@ -28,21 +28,15 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 
-app.set('view engine', 'ejs');
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
-app.use(session({ secret:"abhinav" }));
+app.use(session({ secret:process.env.PASSPORT_SECRET }));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
-
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
-
 //require("./public/assignment/server/app.js")(app);
 
-app.listen(port, ipaddress);
+app.listen(port, ipaddress, function () {
+    console.log("Server is listening on: " + ipaddress + ":" + port);
+});
