185306298f416f041f75fb65fba39b0e59720207
abhinavmaurya <maurya.abhinav20@gmail.com>
2016-03-31 22:44:07 -0400

    deleted id updation in user

--- public/assignment/server/models/user/user.model.js
+++ public/assignment/server/models/user/user.model.js
@@ -54,6 +54,7 @@ module.exports = function(db){
         var deferred = q.defer();
         console.log(userId);
         console.log(user);
+        delete user._id;
         UserModel.update(
             {_id: userId},
             {$set: user},
